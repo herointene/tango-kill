516cac7cada7506810dd847a11743dc7b91576b3
Ted <wulited@outlookcom>
2022-04-20 01:02:08 +0800

    add grid show

---
+++ src/components/MiniWordCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <div
+    class="
+      sm:max-w-fit
+      p-2
+      sm:mr-4
+      mr-2
+      mb-4
+      shadow-xl shadow-slate-700
+      rounded-xl
+      bg-gray-200
+      cursor-pointer
+    "
+    :class="{'bg-yellow-200':starred}"
+    @click="toggleStar"
+  >
+    <div class="flex flex-wrap">
+      <div class="text-3xl font-bold">{{ word[0] }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { StarIcon } from "@heroicons/vue/outline";
+import { StarIcon as SolidStar } from "@heroicons/vue/solid";
+export default {
+  name: "WordCard",
+  components: { StarIcon, SolidStar },
+  props: ["word", "wKey"],
+  data() {
+    return {
+      starred: false,
+      starredArr: JSON.parse(localStorage.getItem("Starred")) || [],
+    };
+  },
+  mounted() {
+    this.starredArr.includes(this.wKey)
+      ? (this.starred = true)
+      : (this.starred = false);
+
+    //if localstorage contains this word, starred; vice versa
+  },
+  methods: {
+    toggleStar() {
+      if (this.starred === false) {
+        this.$emit("star", this.wKey);
+        this.starred = true;
+      } else {
+        this.$emit("unstar", this.wKey);
+        this.starred = false;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/VocaBook/CardDisplay.vue
+++ src/views/VocaBook/CardDisplay.vue
@@ -1,25 +1,63 @@
 <template>
-  <div id="CardDisplay" class="sm:p-16 p-3 flex flex-wrap ">
-    <template v-for="(w, index) in bookShelf[0].content" key="index">
-      <WordCard @star="recordStar" @unstar="delStar" :word="w" :wKey="index" />
-    </template>
+  <div class="sm:p-8 p-3">
+    <div class="h-8 mb-4">
+      <div
+        id="CardTypeSwitch"
+        class="
+          absolute
+          right-3
+          sm:right-8
+          w-16
+          h-8
+          rounded-md
+          bg-slate-700
+          opacity-50
+          grid grid-cols-2
+        "
+      >
+        <div class=" rounded-l-md bg-slate-700 cursor-pointer" :class="{'bg-slate-900': !gridActive}" @click="gridActive=false"></div>
+        <div class=" rounded-r-md bg-slate-700 cursor-pointer" :class="{'bg-slate-900': gridActive}" @click="gridActive=true"></div>
+      </div>
+    </div>
+    <div id="CardDisplay" v-if="gridActive===false" class="flex flex-wrap">
+      <template v-for="(w, index) in bookShelf[0].content" key="index">
+        <WordCard
+          @star="recordStar"
+          @unstar="delStar"
+          :word="w"
+          :wKey="index"
+        />
+      </template>
+    </div>
+    <div id="MiniCardDisplay" v-if="gridActive===true" class="flex flex-wrap">
+      <template v-for="(w, index) in bookShelf[0].content" key="index">
+        <MiniWordCard
+          @star="recordStar"
+          @unstar="delStar"
+          :word="w"
+          :wKey="index"
+        />
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
 import WordCard from "../../components/WordCard.vue";
+import MiniWordCard from "../../components/MiniWordCard.vue";
 import vocaJson from "../../VocabularyJson.json";
 export default {
-  components: { WordCard },
+  components: { WordCard, MiniWordCard },
   data() {
     return {
       vocaJson,
+      gridActive: true
     };
   },
 
   computed: {
     bookShelf() {
-      return vocaJson.vocabulary.book
+      return vocaJson.vocabulary.book;
     },
   },
 
@@ -27,16 +65,16 @@ export default {
     recordStar(wKey) {
       //JSON stringfy
       if (!localStorage.getItem("Starred")) {
-        localStorage.setItem("Starred", JSON.stringify([]))
+        localStorage.setItem("Starred", JSON.stringify([]));
       }
-      let starredArray = JSON.parse(localStorage.getItem("Starred"))
-      starredArray.push(wKey)
-      localStorage.setItem("Starred", JSON.stringify(starredArray))
+      let starredArray = JSON.parse(localStorage.getItem("Starred"));
+      starredArray.push(wKey);
+      localStorage.setItem("Starred", JSON.stringify(starredArray));
     },
     delStar(wKey) {
-      let starredArray = JSON.parse(localStorage.getItem("Starred"))
-      starredArray = starredArray.filter(i => i !== wKey)
-      localStorage.setItem("Starred", JSON.stringify(starredArray))
+      let starredArray = JSON.parse(localStorage.getItem("Starred"));
+      starredArray = starredArray.filter((i) => i !== wKey);
+      localStorage.setItem("Starred", JSON.stringify(starredArray));
     },
   },
 };
